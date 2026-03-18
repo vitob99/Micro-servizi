0a1795e14ea9306a05c17a3b01b5c97e26fe7877
vitob99 <bondanesevt99@gmail.com>
2026-03-18 10:26:27 +0100

    Preparazione

--- scheletro/user-service/obj/Debug/net10.0/UserService.AssemblyInfo.cs
+++ scheletro/user-service/obj/Debug/net10.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35cda8c9219f329ff315c9aad12da7c77647e146")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
